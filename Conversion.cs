d99ab2788233b898e2a1a74a1ae5fb020e4ff1bb
RusselHarveyM <russelharvey.mercado@cit.edu>
2023-10-03 16:58:36 +0800

    make init

---
+++ Conversion.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberSystemConversion
+{
+    public class Conversion
+    {
+        public Conversion() { }
+        public void toBinary(string number, string from)
+        {
+            int[] a = new int[10];
+            int i = 0;
+            switch (from)
+            {
+                case "1":
+                    int newNum = int.Parse(number);
+                    for (i = 0; newNum > 0; i++)
+                    {
+                        a[i] = newNum % 2;
+                        newNum = newNum / 2;
+                    }
+                    Console.Write("Binary of the given number= ");
+                    for (i = i - 1; i >= 0; i--)
+                    {
+                        Console.Write(a[i]);
+                    }
+            
+            break;
+                case "2":
+                    Console.Write("Binary of the given number= ");
+                    Console.Write(number);
+                    break;
+                case "3":
+                    string binary = "";
+                    while (i < number.Length)
+                    {
+                        char c = number[i];
+                        switch (c)
+                        {
+                            case '0':
+                                binary += "000";
+                                break;
+                            case '1':
+                                binary += "001";
+                                break;
+                            case '2':
+                                binary += "010";
+                                break;
+                            case '3':
+                                binary += "011";
+                                break;
+                            case '4':
+                                binary += "100";
+                                break;
+                            case '5':
+                                binary += "101";
+                                break;
+                            case '6':
+                                binary += "110";
+                                break;
+                            case '7':
+                                binary += "111";
+                                break;
+                            default:
+                                System.Console.WriteLine("\nInvalid Octal Digit " + number[i]);
+                                break;
+                        }
+                        i++;
+                    }
+                    System.Console.WriteLine("Binary value is: " + binary);
+                    break;
+                case "4":
+                    break;
+            }
+
+        }
+        public void toOctal(string number, string from) {
+            switch (from)
+            {
+                case "1":
+
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+            }
+        }
+        public void toHexa(string number, string from)
+        {
+            switch (from)
+            {
+                case "1":
+
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+            }
+        }
+        public void toDecimal(string number, string from)
+        {
+            switch (from)
+            {
+                case "1":
+
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using numberSystemConversion;
+
+var conversion = new Conversion();
+
+Console.WriteLine("From Number System: \n[1] Decimal\n[2] Binary\n[3] Octal\n[4] HexaDecimal");
+var fromChoice = Console.ReadLine();
+Console.WriteLine("To Number System: \n[1] Decimal\n[2] Binary\n[3] Octal\n[4] HexaDecimal");
+var toChoice = Console.ReadLine();
+
+Console.WriteLine("Enter number: ");
+var number = Console.ReadLine();
+
+switch(toChoice)
+{
+    case "1":
+        conversion.toDecimal(number, fromChoice);
+        break;
+    case "2":
+        conversion.toBinary(number, fromChoice);
+        break;
+    case "3":
+        conversion.toOctal(number, fromChoice);
+        break;
+    case "4":
+        conversion.toHexa(number, fromChoice);
+        break;
+
+}
+
+
+
+
+
+
+
+
+
